f76db2668685b8f8fbdb132911c0b1f8d3f38fc8
Elza Koylubaeva <zakirova_elza@mail.ru>
2022-08-04 12:29:04 +0500

    Initial commit

---
+++ src/MagicBox.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class MagicBox<T> {
+
+    private final int maxCapacity;
+    private final T[] items;
+    private final Random random = new Random();
+    private int counter = 0;
+
+    @SuppressWarnings("unchecked")
+    public MagicBox(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+        items = (T[]) new Object[maxCapacity];
+    }
+
+    public boolean add(T item) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                items[i] = item;
+                counter++;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T pick() {
+        int countEmptyCells = maxCapacity - counter;
+        if (countEmptyCells != 0) {
+            throw new RuntimeException("The box isn't full. " + countEmptyCells + " cell(s) are empty");
+        } else {
+            int randomInt = random.nextInt(maxCapacity);
+            System.out.println("Return: " + items[randomInt]);
+            return items[randomInt];
+        }
+
+    }
+
+    public void print() {
+        System.out.println(Arrays.toString(items));
+    }
+
+}
+
+
---
+++ src/Main.java
@@ -0,0 +1,41 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        MagicBox<String> magicBoxStr = new MagicBox<>(5);
+        MagicBox<Integer> magicBoxInt = new MagicBox<>(10);
+
+        magicBoxStr.add("Wand");
+        magicBoxStr.add("Cauldron");
+        magicBoxStr.add("Invisibility Cloak");
+        magicBoxStr.add("Potion");
+        magicBoxStr.print();
+        try {
+            magicBoxStr.pick();
+        } catch (RuntimeException e) {
+            System.err.println(e);
+        }
+        magicBoxStr.add("Goblet of Fire");
+        magicBoxStr.print();
+        magicBoxStr.pick();
+
+        magicBoxInt.add(9);
+        magicBoxInt.add(8);
+        magicBoxInt.add(7);
+        magicBoxInt.add(6);
+        magicBoxInt.add(5);
+        magicBoxInt.add(4);
+        magicBoxInt.add(3);
+        magicBoxInt.add(2);
+        magicBoxInt.add(1);
+        magicBoxInt.print();
+        try {
+            magicBoxInt.pick();
+        } catch (RuntimeException e) {
+            System.err.println(e);
+        }
+        magicBoxInt.add(0);
+        magicBoxInt.print();
+        magicBoxInt.pick();
+    }
+}
